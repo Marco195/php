49cbad1cde1592fdcad144bf634fc743c782a51c
Marco Aurelio P Mondini <marco-aurelio.pm@hotmail.com>
2018-07-10 18:22:52 -0300

    Commit de teste

---
+++ index.php
@@ -0,0 +1,28 @@
+<html lang='pt-br'>
+    <head>
+        <meta charset="UTF-8">
+        <link rel='stylesheet' href='style.css'>
+        <title>I'm Batman</title>
+    </head>
+
+            <ul>
+                <li><a class="ativo" href="index.php">Home</a></li>
+                <li><a href="#news">News</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#about">About</a></li>
+            </ul>
+        
+              
+        <hr>
+        
+        <div class="hello">
+        <p><strong>Hello World!</strong></p>
+        </div>
+        
+        
+            <?php
+            echo "Hello world";
+            ?>
+        
+    </body>
+</html>
